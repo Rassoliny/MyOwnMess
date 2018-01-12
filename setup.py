244e2ca927de083c4b992dea7f1d407593ab0f8c
Дмитрий Коваленко <dimo120@yandex.ru>
2018-01-12 11:02:44 +0300

    Changes in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
  
-setup(name = "myownmessenger",
-      version = "0.1",
+setup(name = "myownmessenger_test",
+      version = "0.1.1",
       description = "My first messenger",
       author = "Rassoliny",
       author_email = "dimo120@yandex.ru",
       url = "https://github.com/Rassoliny/MyOwnMess",
-      packages=["my_own_server", "my_own_jim", "my_own_repo", "my_own_logs", "my_own_client", "my_own_messenger"]
+      packages=["my_own_server", "my_own_jim", "my_own_repo", "my_own_logs", "my_own_client", "my_own_messenger"],
+      python_requires='>=3.6',
+      install_requires=[
+            'PyQt5==5.9', 'SQLAlchemy==1.1.15'
+      ],
       )
