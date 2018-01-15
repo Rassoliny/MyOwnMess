44ac860eda3b86c45f8348cb36cff16a20ac672f
Дмитрий Коваленко <dimo120@yandex.ru>
2018-01-15 22:46:34 +0300

    Image in wheel

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
+from glob import glob
  
 setup(name = "myownmessenger_test",
       version = "0.1.1",
       description = "My first messenger",
+      include_package_data=True,
       author = "Rassoliny",
       author_email = "dimo120@yandex.ru",
       url = "https://github.com/Rassoliny/MyOwnMess",
       packages=["my_own_server", "my_own_jim", "my_own_repo", "my_own_logs", "my_own_client", "my_own_messenger"],
-      python_requires='>=3.6',
+      data_files=[
+        ('image', glob('my_own_messenger/*.png')),
+    ],python_requires='>=3.6',
       install_requires=[
             'PyQt5==5.9', 'SQLAlchemy==1.1.15'
       ],
